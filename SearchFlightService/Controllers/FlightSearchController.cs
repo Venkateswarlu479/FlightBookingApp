7a1c0f1ed1ab3f4fc6d06bc60ad0ff91a8554e46
cogdotnet913 <cogdotnet913@CTSDOTNET278>
2022-06-21 17:04:25 +0530

    Add project files.

---
+++ SearchFlightService/Controllers/FlightSearchController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchFlightService.Controllers
+{
+    [Route("api/v1.0/flight")]
+    [ApiController]
+    public class FlightSearchController : ControllerBase
+    {
+        [HttpPost("Search")]
+        public string SearchFlight()
+        {
+            return "No flights found at the moment";
+        }
+    }
+}
